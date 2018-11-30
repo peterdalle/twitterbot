b47a29678ddf386f832937ba07970d1f93dca623
Peter M. Dahlgren <peterdalle@users.noreply.github.com>
2018-11-30 16:45:10 +0100

    Refactored to follow Python style guide

--- twitterbot.py
+++ twitterbot.py
@@ -1,154 +1,129 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# Get RSS feed items from http://example.net/feed/ and post tweets to @youraccount.
-# By Peter M. Dahlgren, @peterdalle https://github.com/peterdalle/twitterbot/
-
 from twython import Twython, TwythonError
+import feedparser
 import csv
-import sys
+import datetime
 import os
 import re
+import sys
 import time
-import datetime
-import feedparser
 from datetime import date
 
-# Settings for the application.
 class Settings:
-	FeedUrl = "http://example.net/feed/"			# RSS feed to read and post tweets from.
-	PostedUrlsOutputFile = "posted-urls.log"		# Log file to save all tweeted RSS links (one URL per line).
-	PostedRetweetsOutputFile = "posted-retweets.log"	# Log file to save all retweeted tweets (one tweetid per line).
-	RetweetIncludeWords = ["#hashtag"]			# Include tweets with these words when retweeting.
-	RetweetExcludeWords = []				# Do not include tweets with these words when retweeting.
+	"""
+	Twitter bot application settings.
+	
+	Enter the RSS feed you want to tweet, or keywords you want to retweet.
+	"""
+	FeedUrl = "http://example.net/feed/"               # RSS feed to read and post tweets from.
+	PostedUrlsOutputFile = "posted-urls.log"           # Log file to save all tweeted RSS links (one URL per line).
+	PostedRetweetsOutputFile = "posted-retweets.log"   # Log file to save all retweeted tweets (one tweetid per line).
+	RetweetIncludeWords = ["#hashtag"]                 # Include tweets with these words when retweeting.
+	RetweetExcludeWords = []                           # Do not include tweets with these words when retweeting.
 
-# Twitter authentication settings. Create a Twitter app at https://apps.twitter.com/ and
-# generate key, secret etc, and insert them below.
 class TwitterAuth:
+	"""
+	Twitter authentication settings.
+
+	Create a Twitter app at https://apps.twitter.com/ and generate key, secret etc. and insert them here.
+	"""
 	ConsumerKey = "XXX"
 	ConsumerSecret = "XXX"
 	AccessToken = "XXX"
 	AccessTokenSecret = "XXX"
 
-# Post tweet to account.
-def PostTweet(title, link):
-	# Truncate title and append ... at the end if length exceeds 250 chars (which is enough space for a t.co link).
-	title = (title[:250] + '...') if len(title) > 250 else title
-	message = title + " " + link
+def compose_message(rss_item):
+	"""Compose a tweet from title, link, and description, and then return the final tweet message."""
+	title, link, description = rss_item["title"], rss_item["link"], rss_item["description"]
+	message = shorten_text(title, maxlength=250) + " " + link
+	return message
+
+def shorten_text(text, maxlength):
+	"""Truncate text and append three dots (...) at the end if length exceeds maxlength chars."""
+	return (text[:maxlength] + '...') if len(text) > maxlength else text
+
+def post_tweet(message):
+	"""Post tweet message to account."""
 	try:
-		# Tweet message.
-		twitter = Twython(TwitterAuth.ConsumerKey, TwitterAuth.ConsumerSecret, TwitterAuth.AccessToken, TwitterAuth.AccessTokenSecret) # Connect to Twitter.
+		twitter = Twython(TwitterAuth.ConsumerKey, TwitterAuth.ConsumerSecret, TwitterAuth.AccessToken, TwitterAuth.AccessTokenSecret)
 		twitter.update_status(status = message)
 	except TwythonError as e:
 		print(e)
 
-# Read RSS and post tweet.
-def ReadRssAndTweet(url):
+def read_rss_and_tweet(url):
+	"""Read RSS and post tweet."""
 	feed = feedparser.parse(url)
-	for item in feed["items"]:
-		title = item["title"]
-		link = item["link"]
-		# Make sure we don't post any dubplicates.
-		if not (IsUrlAlreadyPosted(link)):
-			PostTweet(title, link)
-			MarkUrlAsPosted(link)
-			print("Posted: " + link)
-		else:
-			print("Already posted: " + link)
-			
-# Has the URL already been posted? 
-def IsUrlAlreadyPosted(url):
-	if os.path.isfile(Settings.PostedUrlsOutputFile):
-		# Check whether URL is in log file.
-		f = open(Settings.PostedUrlsOutputFile)
-		posted_urls = f.readlines()
-		f.close()
-		if (url + "\n" or url) in posted_urls:
-			return(True)
-		else:
-			return(False)
-	else:
-		return(False)
-
-# Mark the specific URL as already posted.
-def MarkUrlAsPosted(url):
-	try:
-		# Write URL to log file.
-		f = open(Settings.PostedUrlsOutputFile, "a")
-		f.write(url + "\n")
-		f.close()
-	except:
-		print("Write error:", sys.exc_info()[0])
-
-# Search for particular keywords in tweets and retweet those tweets.
-def SearchAndRetweet():
-	# Create Twitter search query with included words minus the excluded words.
-	filter = " OR ".join(Settings.RetweetIncludeWords)
-	blacklist = " -".join(Settings.RetweetExcludeWords)
-	keywords = filter + blacklist
-
-	# Connect to Twitter.
-	twitter = Twython(TwitterAuth.ConsumerKey, TwitterAuth.ConsumerSecret, TwitterAuth.AccessToken, TwitterAuth.AccessTokenSecret)
-	search_results = twitter.search(q=keywords, count=10)
-	try:
-		for tweet in search_results["statuses"]:
-			# Make sure we don't retweet any dubplicates.
-			if not IsTweetAlreadyRetweeted(tweet["id_str"]):
-				try:
-					twitter.retweet(id = tweet["id_str"])
-					MarkTweetAsRetweeted(tweet["id_str"])
-					print("Retweeted " + tweet["text"].encode("utf-8") + " (tweetid " + str(tweet["id_str"]) + ")")
-				except TwythonError as e:
-					print(e)
+	if feed:
+		for item in feed["items"]:
+			link = item["link"]
+			if is_in_logfile(link, Settings.PostedUrlsOutputFile):
+				print("Already posted:", link)
 			else:
-				print("Already retweeted " + tweet["text"].encode("utf-8") + " (tweetid " + str(tweet["id_str"]) + ")")
+				post_tweet(message = compose_message(item))
+				write_to_logfile(link, Settings.PostedUrlsOutputFile)
+				print("Posted:", link)
+	else:
+		print("Nothing found in feed", url)
+
+def get_query():
+	"""Create Twitter search query with included words minus the excluded words."""
+	include = " OR ".join(Settings.RetweetIncludeWords)
+	exclude = " -".join(Settings.RetweetExcludeWords)
+	return include + exclude
+
+def search_and_retweet(query, count=10):
+	"""Search for a query in tweets, and retweet those tweets."""
+	try:
+		twitter = Twython(TwitterAuth.ConsumerKey, TwitterAuth.ConsumerSecret, TwitterAuth.AccessToken, TwitterAuth.AccessTokenSecret)
+		search_results = twitter.search(q=query, count=count)
 	except TwythonError as e:
 		print(e)
-
-# Has the tweet already been retweeted? 
-def IsTweetAlreadyRetweeted(tweetid):
-	if os.path.isfile(Settings.PostedRetweetsOutputFile):
-		# Check whether tweet IDs is in log file.
-		f = open(Settings.PostedRetweetsOutputFile)
-		posted_tweets = f.readlines()
-		f.close()
-		if (tweetid + "\n" or tweetid) in posted_tweets:
-			return(True)
+	for tweet in search_results["statuses"]:
+		# Make sure we don't retweet any dubplicates.
+		if not is_in_logfile(tweet["id_str"], Settings.PostedRetweetsOutputFile):
+			try:
+				twitter.retweet(id = tweet["id_str"])
+				write_to_logfile(tweet["id_str"], Settings.PostedRetweetsOutputFile)				
+				print("Retweeted {} (id {})".format(shorten_text(tweet["text"], maxlength=40), tweet["id_str"]))
+			except TwythonError as e:
+				print(e)
 		else:
-			return(False)
-	else:
-		return(False)
+			print("Already retweeted {} (id {})".format(shorten_text(tweet["text"], maxlength=40), tweet["id_str"]))
 
-# Mark the specific tweet as already retweeted.
-def MarkTweetAsRetweeted(tweetid):
+def is_in_logfile(content, filename):
+	"""Does the content exist on any line in the log file?"""
+	if os.path.isfile(filename):
+		with open(filename) as f:
+			lines = f.readlines()
+		if (content + "\n" or content) in lines:
+			return True
+	return False
+	
+def write_to_logfile(content, filename):
+	"""Append content to log file, on one line."""
 	try:
-		# Write tweet ID to log file.
-		f = open(Settings.PostedRetweetsOutputFile, "a")
-		f.write(tweetid + "\n")
-		f.close()
-	except:
-		print("Write error:", sys.exc_info()[0])
+		with open(filename, "a") as f:
+			f.write(content + "\n")
+	except IOError as e:
+		print(e)
 
-# Show available commands.
-def DisplayHelp():
-	print("Syntax: python twitterbot.py [cmd]")
-	print
-	print(" Available commands:")
+def display_help():
+	"""Show available commands."""
+	print("Syntax: python {} [command]".format(sys.argv[0]))
+	print()
+	print(" Commands:")
 	print("    rss    Read URL and post new items to Twitter account (change account in source code)")
 	print("    rt     Search and retweet keywords (see source code for keywords)")
 	print("    help   Show this help screen")
-	print
 
-# Main.
-if (__name__ == "__main__"):
+if __name__ == "__main__":
 	if len(sys.argv) > 1:
-		cmd = sys.argv[1]
-		if (cmd == "rss"):
-			ReadRssAndTweet(Settings.FeedUrl)
-		elif (cmd == "rt"):
-			SearchAndRetweet()
+		if sys.argv[1].lower() == "rss":
+			read_rss_and_tweet(url=Settings.FeedUrl)
+		elif sys.argv[1].lower() == "rt":
+			search_and_retweet(query=get_query())
 		else:
-			DisplayHelp()
+			display_help()
 	else:
-		DisplayHelp()
-		sys.exit()
+		display_help()
